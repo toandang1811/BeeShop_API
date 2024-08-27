3d9cc5d90e019ea20c50c3892e06170f3653abc4
toandang1811 <dvt12209@gmail.com>
2024-08-27 23:16:44 +0700

    ...

---
+++ BeeShop_API.Repositories/Contracts/Mappers/IUserMapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeShop_API.Repositories.Contracts.Mappers
+{
+    public interface IUserMapper
+    {
+        Domain.Entities.User FillFromDataAccess(DataAccess.User user);
+        DataAccess.User FillFromDomain(Domain.Entities.User user);
+    }
+}
---
+++ BeeShop_API.Repositories/Contracts/Mappers/IUserSessionMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeShop_API.Repositories.Contracts.Mappers
+{
+    public interface IUserSessionMapper
+    {
+        DataAccess.UserSession FillFromDomain(Domain.Entities.UserSession userSession);
+    }
+}
